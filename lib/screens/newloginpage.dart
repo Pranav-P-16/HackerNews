da2640556d693f02a7c1fff46431d03ab8c5fef0
Pranav_16 <pranavpraveen268@gmail.com>
2024-04-11 13:31:13 +0530

    Initial Commit

---
+++ lib/screens/newloginpage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class NewLoginPage extends StatefulWidget {
+  const NewLoginPage({super.key});
+
+  @override
+  State<NewLoginPage> createState() => _NewLoginPageState();
+}
+
+class _NewLoginPageState extends State<NewLoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
--- lib/topArticleList.dart
+++ lib/topArticleList.dart
@@ -132,7 +132,7 @@ class _TopArticleListState extends State<TopArticleList> {
                         )
                     ),
                   ),
-                ),
+                ), 
               );
             },
           )],
